f9e1cdbf97d7394adc91b60a5334b5f127963e73
khorshed31 <khorshedalom3517@gmail.com>
2022-01-27 13:13:29 +0600

    php setup

--- app/classes/HelloWorld.php
+++ app/classes/HelloWorld.php
@@ -6,5 +6,21 @@ namespace App\classes;
 
 class HelloWorld
 {
+    public $name = 'bitm';
+    public function index(){
+        // Variable
+       /* 3 major rules
+           # start with $
+           # a-z, A-Z, 0-9
+           # no number in first
+       */
 
+        /*
+        3 standard rules for variable
+          # small letter
+          # meaning full
+          # readable
+
+        */
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php
+require_once 'vendor/autoload.php';
+use App\classes\HelloWorld;
+
+$helloWorld = new HelloWorld();
+$helloWorld->index();
